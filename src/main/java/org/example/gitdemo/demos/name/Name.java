cbe6731d6164741c540fe00b2f5961f028555ae0
zoujun <11885329+mennmen@user.noreply.gitee.com>
2025-06-15 23:14:22 +0800

    第二次修改

--- src/main/java/org/example/gitdemo/demos/name/Name.java
+++ src/main/java/org/example/gitdemo/demos/name/Name.java
@@ -1,6 +1,7 @@
  public class Name {
 
     public static final string NAME = "John";
+    public static final int AGE = 25;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
